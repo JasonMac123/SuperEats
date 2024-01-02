d7d31b4ef56376b2734c968a77df45734d960ec8
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-02 15:04:22 -0500

    Adds tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: ["./App.{js,jsx,ts,tsx}", "./**/*.{js, jsx, ts,tsx}"],
+  },
+  plugins: [],
+};
