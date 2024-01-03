02bfaef157b5fad964609ff3db7b3567fb164e33
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-03 17:21:22 -0500

    Creates homescreen layout for header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+import tw from "twrnc";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={tw`flex-row pb-3 items-center mx-4 gap-2`}>
+        <Ionicons name="fast-food" size={40} color="green" />
+        <View>
+          <Text>Deliver now!</Text>
+          <Text>Current Location</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
