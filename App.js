5a21e3d0735ef43904913d157e9dfda30c2968d0
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-03 17:28:13 -0500

    Fixes bug where status bar color for user would not change

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { StatusBar } from "expo-status-bar";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -8,6 +10,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
