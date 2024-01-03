4928fa2281cd1faf1797781653bc06f81a821e68
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-03 18:13:54 -0500

    Adds styling to category card

--- components/Category/CategoryCard.js
+++ components/Category/CategoryCard.js
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
+import { Image, Text, TouchableOpacity } from "react-native";
 import React from "react";
+
+import tw from "twrnc";
 
 const CategoryCard = ({ img, title }) => {
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <TouchableOpacity style={tw`relative mr-2`}>
+      <Image source={{ uri: img }} style={tw`h-20 w-20 rounded`} />
+      <Text style={tw`abssolute bottom-1 left-1 text-white font-bold`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
