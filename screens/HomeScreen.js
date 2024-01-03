738ed3ca7941bc10ef9eb4396e39186fd88cd774
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-03 17:37:45 -0500

    Adds search bar layout and styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,12 @@
 import React, { useLayoutEffect } from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, TextInput } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+r;
+import { FontAwesome5 } from "@expo/vector-icons";
+
 import { useNavigation } from "@react-navigation/native";
 
 import tw from "twrnc";
@@ -16,13 +21,26 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw`bg-white pt-5`}>
       <View style={tw`flex-row pb-3 items-center mx-4 gap-2`}>
         <Ionicons name="fast-food" size={40} color="green" />
-        <View>
-          <Text>Deliver now!</Text>
-          <Text>Current Location</Text>
+        <View style={tw`flex-1`}>
+          <Text style={tw`font-bold text-gray-400 text-xs`}>Deliver now!</Text>
+          <Text style={tw`font-bold text-xl`}>
+            Current Location <AntDesign name="down" size={18} color="#00CCB8" />
+          </Text>
         </View>
+        <FontAwesome5 name="user-alt" size={24} color="black" />
+      </View>
+      <View>
+        <View style={tw`flex-row gap-2 flex-1 bg-gray-200 p-3`}>
+          <FontAwesome name="search" size={24} color="black" />
+          <TextInput
+            placeholder="Search food places here!"
+            keyboardType="default"
+          />
+        </View>
+        <Ionicons name="settings" size={24} color="black" />
       </View>
     </SafeAreaView>
   );
