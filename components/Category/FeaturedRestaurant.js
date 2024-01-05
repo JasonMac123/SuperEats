97409c42069da35ee485908aaf0968e4183f81bb
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-04 19:07:09 -0500

    Changes base layout for featured restaurant

--- components/Category/FeaturedRestaurant.js
+++ components/Category/FeaturedRestaurant.js
@@ -1,10 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import { AntDesign } from "@expo/vector-icons";
+import tw from "twrnc";
+
 const FeaturedRestaurant = ({ title, description, img }) => {
   return (
     <View>
-      <Text>FeaturedRow</Text>
+      <View style={tw`mt-4 flex-row items-center justify-between px-4`}>
+        <Text style={tw`font-bold text-lg`}>{title}</Text>
+        <AntDesign name="arrowright" size={24} color="black" />
+      </View>
+      <Text style={tw`text-xs text-gray-500 px-4`}>{description}</Text>
     </View>
   );
 };
