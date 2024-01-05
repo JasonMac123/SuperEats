adaef09c070d6ba19568419f79d0983b3c188334
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-05 15:28:00 -0500

    Creates all screens for mobile app

--- screens/NavigationBar.jsx
+++ screens/NavigationBar.jsx
@@ -1,13 +1,92 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
 import HomeScreen from "./HomeScreen";
+import SearchScreen from "./SearchScreen";
+import ProfileScreen from "./ProfileScreen";
+import CartScreen from "./CartScreen";
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions = {
+  tabBarShowLabel: false,
+  tabBarHideOnKeyboard: true,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 70,
+  },
+};
+
 const NavigationBar = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: (focused) => {
+            return (
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: (focused) => {
+            return (
+              <Ionicons
+                name={"search"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: (focused) => {
+            return (
+              <FontAwesome
+                name={"user"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: (focused) => {
+            return (
+              <Ionicons
+                name={"cart-sharp"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
