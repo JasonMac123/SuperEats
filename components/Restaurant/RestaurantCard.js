f3f397f67b872a07781b1f5ff260be0d19f51d7d
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-04 19:24:12 -0500

    Creates layout for restaurant card information and styles card information

--- components/Restaurant/RestaurantCard.js
+++ components/Restaurant/RestaurantCard.js
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
+
+import { FontAwesome } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
+import tw from "twrnc";
 
 const RestaurantCard = ({
   id,
-  imgUrl,
+  img,
   title,
   rating,
   genre,
@@ -14,9 +19,27 @@ const RestaurantCard = ({
   lat,
 }) => {
   return (
-    <View>
-      <Text>RestaurantCard</Text>
-    </View>
+    <TouchableOpacity>
+      <Image
+        source={{
+          uri: img,
+        }}
+        style={tw`h-36 w-64 rounded-md`}
+      />
+      <View style={tw`px-3 pb-4`}>
+        <Text style={tw`font-bold text-lg pt-2`}>{title}</Text>
+        <View style={tw`flex-row items-center gap-2`}>
+          <FontAwesome name="star-o" size={24} color="yellow" />
+          <Text style={`text-xs text-gray-500`}>
+            <Text style={tw`text-yellow-700`}>{rating}</Text> - {genre}
+          </Text>
+        </View>
+        <View style={tw`flex-row items-center gap-2`}>
+          <Entypo name="location" size={24} color="black" />
+          <Text style={tw`text-xs`}>{address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
