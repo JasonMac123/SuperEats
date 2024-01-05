1a8b5d8e6c9a3626488d1efdff6d6c3ed31db73e
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-05 15:46:57 -0500

    Refactors header component and seperates search bar into own file

---
+++ components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { View, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
+
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+import tw from "twrnc";
+
+const SearchBar = () => {
+  return (
+    <View style={tw`flex-row items-center gap-2 pb-2 mx-4`}>
+      <View style={tw`flex-row flex-1 gap-2 bg-gray-200 p-3`}>
+        <FontAwesome name="search" size={24} color="black" />
+        <TextInput
+          placeholder="Search food places here!"
+          keyboardType="default"
+        />
+      </View>
+      <TouchableOpacity>
+        <Ionicons name="options-outline" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchBar;
