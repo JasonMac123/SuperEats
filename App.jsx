eae0e110c4cf5fa5241bbdd965afcdc824b27cca
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-06 20:51:23 -0500

    Adds bottom sheet modal provider into app

--- App.jsx
+++ App.jsx
@@ -3,8 +3,8 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from "./screens/HomeScreen";
 import NavigationBar from "./screens/NavigationBar";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,13 +12,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Bottom Navigation"
-          component={NavigationBar}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <BottomSheetModalProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Bottom Navigation"
+            component={NavigationBar}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </BottomSheetModalProvider>
     </NavigationContainer>
   );
 }
