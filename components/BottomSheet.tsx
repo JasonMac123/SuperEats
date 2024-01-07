cf4fdacf7ce66d72802f088b4e9285db04ccb7ab
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-06 21:40:15 -0500

    Adds backdrop feature to bottom sheet modal

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,14 +1,36 @@
-import React, { useMemo, forwardRef } from "react";
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import React, { useMemo, forwardRef, useCallback } from "react";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  useBottomSheetModal,
+} from "@gorhom/bottom-sheet";
 import { View, Text } from "react-native";
 
 export type Ref = BottomSheetModal;
 
 const BottomSheet = forwardRef<Ref>((props, ref) => {
-  const snapPoints = useMemo(() => ["50%"], []);
+  const snapPoints = useMemo(() => ["55%"], []);
+
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        {...props}
+      />
+    ),
+    []
+  );
+
+  const { dismiss } = useBottomSheetModal();
 
   return (
-    <BottomSheetModal ref={ref} snapPoints={snapPoints}>
+    <BottomSheetModal
+      ref={ref}
+      snapPoints={snapPoints}
+      overDragResistanceFactor={0}
+      backdropComponent={renderBackdrop}
+    >
       <View>
         <Text>BottomSheet</Text>
       </View>
