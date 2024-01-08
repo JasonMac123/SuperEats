0b9564288b08c0e7906e2eb5cd50a7fb16c9dd12
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-08 16:00:13 -0500

    Fixes styling for filter button to have elevation and shadow on top

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,14 +1,20 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 
 import tw from "twrnc";
 
 const FilterModal = () => {
   return (
-    <View style={tw`p-8 bg-white relative h-full`}>
-      <View style={tw`absolute bottom-8 w-80 h-16 mx-8`}>
+    <View style={tw`p-8 bg-white`}>
+      <View style={styles.footer}>
         <TouchableOpacity
-          style={tw`flex-1 items-center justify-center bg-green-700 rounded-md`}
+          style={tw`h-16 mx-4 items-center justify-center bg-green-700 rounded-md`}
           onPress={() => {}}
         >
           <Text style={tw`text-white text-xl font-bold`}>Filter</Text>
@@ -19,3 +25,18 @@ const FilterModal = () => {
 };
 
 export default FilterModal;
+
+const styles = StyleSheet.create({
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 120,
+    backgroundColor: "#FFF",
+    padding: 10,
+    elevation: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+  },
+});
