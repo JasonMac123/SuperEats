0b28096d9bbff588eead8a249a5dc2bf077936f0
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-08 15:29:27 -0500

    Adds styling for filter modal filter button

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+
+import tw from "twrnc";
 
 const FilterModal = () => {
   return (
-    <View>
-      <Text>FilterModal</Text>
+    <View style={tw`p-8 bg-white relative h-full`}>
+      <View style={tw`absolute bottom-8 w-80 h-16 mx-8`}>
+        <TouchableOpacity
+          style={tw`flex-1 items-center justify-center bg-green-700 rounded-md`}
+          onPress={() => {}}
+        >
+          <Text style={tw`text-white text-xl font-bold`}>Filter</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
