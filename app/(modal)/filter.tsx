3d5e33b5671215608968693caaf79cab1398a8d7
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-08 16:55:05 -0500

    Adds filter inputs and filter category lists

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -2,20 +2,45 @@ import {
   View,
   Text,
   TouchableOpacity,
-  ScrollView,
   StyleSheet,
+  FlatList,
+  ListRenderItem,
 } from "react-native";
 import React from "react";
+import { useNavigation } from "expo-router";
 
 import tw from "twrnc";
+import { Ionicons } from "@expo/vector-icons";
+import ItemBox from "../../components/ItemBox";
+
+interface Category {
+  name: string;
+  count: number;
+  checked?: boolean;
+}
 
 const FilterModal = () => {
+  const navigation = useNavigation();
+
+  const renderItem: ListRenderItem<Category> = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={tw`p-8 bg-white`}>
+      <FlatList
+        data={[]}
+        renderItem={renderItem}
+        ListHeaderComponent={<ItemBox />}
+      />
       <View style={styles.footer}>
         <TouchableOpacity
           style={tw`h-16 mx-4 items-center justify-center bg-green-700 rounded-md`}
-          onPress={() => {}}
+          onPress={() => navigation.goBack()}
         >
           <Text style={tw`text-white text-xl font-bold`}>Filter</Text>
         </TouchableOpacity>
