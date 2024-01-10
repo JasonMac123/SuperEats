4f73c5b0c591f7b69d5be98881bf321aaa2065c1
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-10 16:14:20 -0500

    Renders restaurant card for all restaurants

--- components/Restaurant/Restaurants.tsx
+++ components/Restaurant/Restaurants.tsx
@@ -1,11 +1,44 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Text, View, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+
+import tw from "twrnc";
+
+interface Restaurant {
+  name: string;
+  img: any;
+  lat: number;
+  long: number;
+  rating: number;
+  meals: Meal[];
+}
+
+interface Meal {
+  name: string;
+  price: number;
+  img: any;
+}
 
 const Restaurants = () => {
+  const [restaurantData, setrestaurantData] = useState<Restaurant[]>([]);
+
   return (
-    <View>
-      <Text>Restaurants</Text>
-    </View>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={tw`gap-4 bg-white`}
+      contentContainerStyle={{
+        padding: 15,
+      }}
+    >
+      {restaurantData.map((restaurant: Restaurant) => (
+        <View key={restaurant.name} style={tw`mx-1`}>
+          <Image source={restaurant.img} />
+          <Text style={tw`font-semibold text-sm text-center shadow-md`}>
+            {restaurant.name}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
