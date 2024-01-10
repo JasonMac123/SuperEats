c6b1a91ff072ffea3a6482a02a668334cda62dca
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-10 15:38:09 -0500

    Adds category image and styling to each category card

--- components/Category/Categories.tsx
+++ components/Category/Categories.tsx
@@ -1,19 +1,26 @@
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View, Text, Image } from "react-native";
 import React from "react";
+
+import tw from "twrnc";
 
 import { categories } from "../../constants/data";
 
 interface Category {
   name: string;
-  img: string;
+  img: any;
 }
 
 const Categories = () => {
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={tw`gap-4`}
+    >
       {categories.map((category: Category) => (
-        <View key={category.name}>
-          <Text>{category.name}</Text>
+        <View key={category.name} style={tw`mx-4 h-20 w-20`}>
+          <Image source={category.img} />
+          <Text style={tw`font-semibold text-md`}>{category.name}</Text>
         </View>
       ))}
     </ScrollView>
