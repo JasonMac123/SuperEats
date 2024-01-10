e9c12d181eb1134e58c8728b024b7c8d9c93971f
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-10 15:22:53 -0500

    Adds all constant data and images for categories into asset folders

---
+++ constants/data/index.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    name: "Restaurants",
+    img: require("@/assets/images/restaurants.png"),
+  },
+  {
+    name: "Groceries",
+    img: require("@/assets/images/groceries.png"),
+  },
+  {
+    name: "Offers",
+    img: require("@/assets/images/offers.png"),
+  },
+  {
+    name: "Pickup",
+    img: require("@/assets/images/pickup.png"),
+  },
+];
