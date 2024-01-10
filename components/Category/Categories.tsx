b2e589934fc4509bb3cbc5b8939edc859ed5e175
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-10 15:25:44 -0500

    Adds category data and displays each category data

--- components/Category/Categories.tsx
+++ components/Category/Categories.tsx
@@ -1,8 +1,23 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 import React from "react";
 
+import { categories } from "../../constants/data";
+
+interface Category {
+  name: string;
+  img: string;
+}
+
 const Categories = () => {
-  return <></>;
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {categories.map((category: Category) => (
+        <View key={category.name}>
+          <Text>{category.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
 };
 
 export default Categories;
