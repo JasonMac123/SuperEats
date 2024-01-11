f62aaf360b718c286a08d7cb9240481d0ec52e8b
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-11 15:40:46 -0500

    Creates function to set user position to use for restaurant distance calculation

--- app/(modal)/search.tsx
+++ app/(modal)/search.tsx
@@ -1,7 +1,28 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import usePosition from "../../hooks/usePosition";
+import * as Location from "expo-location";
 
 const search = () => {
+  const { latitude, longitude, setPosition } = usePosition();
+  const [permissions, setPermissions] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+      setPermissions(true);
+      let currentPosition = await Location.getCurrentPositionAsync({});
+      setPosition(
+        currentPosition.coords.latitude,
+        currentPosition.coords.longitude
+      );
+    };
+  });
+
   return (
     <View>
       <Text>search-location</Text>
