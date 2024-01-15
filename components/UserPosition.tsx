08424cd23f84f30781bbed46fd2660ec1e55d341
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-14 21:31:12 -0500

    Styles address text input component

--- components/UserPosition.tsx
+++ components/UserPosition.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 
 import usePosition from "../hooks/usePosition";
 import * as Location from "expo-location";
+
+import { AntDesign } from "@expo/vector-icons";
+
+import tw from "twrnc";
 
 const UserPosition = () => {
   const { latitude, longitude, setPosition } = usePosition();
@@ -30,8 +34,18 @@ const UserPosition = () => {
   };
 
   return (
-    <View>
-      <Text>search-location</Text>
+    <View style={tw`mb-4`}>
+      <Text style={tw`text-lg ml-10`}>Address</Text>
+      <View style={tw`flex-row bg-neutral-200 py-4 px-4 items-center mx-8`}>
+        <TextInput
+          placeholder="Enter your address"
+          keyboardType="default"
+          style={tw`flex-1 font-semibold`}
+        />
+        <TouchableOpacity>
+          <AntDesign name="arrowright" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
