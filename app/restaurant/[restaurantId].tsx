f93dc42f774a397e8cfac5fb34a7ca047b1a7b37
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-15 17:00:52 -0500

    Creates method to grab restaurant data and display it otherwise a loading screen will appear

--- app/restaurant/[restaurantId].tsx
+++ app/restaurant/[restaurantId].tsx
@@ -1,9 +1,32 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { SplashScreen, useLocalSearchParams } from "expo-router";
+import { Restaurant } from "../../constants/types";
+
+import firebase_app from "../../firebase/config";
+import { getFirestore, getDoc, doc } from "firebase/firestore";
 
 const RestaurantIdPage = () => {
   const { restaurantId } = useLocalSearchParams();
+  const id = restaurantId as string;
+
+  const [restaurantData, setRestaurantData] = useState<Restaurant | Object>({});
+
+  useEffect(() => {
+    const db = getFirestore(firebase_app);
+
+    getDoc(doc(db, "restaurants", id))
+      .then((data: any) => {
+        setRestaurantData({ ...data.data(), id: data.id });
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!restaurantData) {
+    return <SplashScreen />;
+  }
 
   return (
     <View>
