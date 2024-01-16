9a8abf8dd8e9fbdea9be9aaf8fb8a10b68b4860f
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-16 18:48:27 -0500

    Adds header icons for navigation and user functionality to share or search

--- components/Parallax/ParallaxHeader.tsx
+++ components/Parallax/ParallaxHeader.tsx
@@ -1,8 +1,11 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 
 import ParallaxScrollView from "./ParallaxScrollView";
 import { useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+import tw from "twrnc";
 
 interface ParallaxHeaderProps {
   img: string;
@@ -14,6 +17,30 @@ const ParallaxHeader = ({ img }: ParallaxHeaderProps) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTransparent: true,
+      headerTitle: "",
+      headerTintColor: "#15803d",
+      headerLeft: () => (
+        <TouchableOpacity
+          style={tw`w-12 h-12 rounded-full bg-white justify-center items-center`}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="arrow-back" color="#15803d" size={24} />
+        </TouchableOpacity>
+      ),
+      headerRight: () => {
+        <View style={tw`flex-row items-center justify-center gap-4`}>
+          <TouchableOpacity
+            style={tw`w-12 h-12 rounded-full bg-white justify-center items-center`}
+          >
+            <Ionicons name="share-outline" color="#15803d" size={24} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={tw`w-12 h-12 rounded-full bg-white justify-center items-center`}
+          >
+            <Ionicons name="search-outline" color="#15803d" size={24} />
+          </TouchableOpacity>
+        </View>;
+      },
     });
   });
 
