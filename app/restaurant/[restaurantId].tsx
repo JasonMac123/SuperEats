853c4d0ce68a6814e13e6fd2085f608834273c5e
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-16 19:11:15 -0500

    Fixes bug where firebase was not returning restaurant data

--- app/restaurant/[restaurantId].tsx
+++ app/restaurant/[restaurantId].tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SplashScreen, useLocalSearchParams } from "expo-router";
 import { Restaurant } from "../../constants/types";
@@ -17,9 +17,9 @@ const RestaurantIdPage = () => {
   useEffect(() => {
     const db = getFirestore(firebase_app);
 
-    getDoc(doc(db, "restaurants", id))
-      .then((data: any) => {
-        setRestaurantData({ ...data.data(), id: data.id });
+    getDoc(doc(db, "restuarants", id))
+      .then((doc: any) => {
+        setRestaurantData({ ...doc.data(), id: doc.id });
       })
       .catch((error: any) => {
         console.log(error);
@@ -27,14 +27,20 @@ const RestaurantIdPage = () => {
   }, []);
 
   if (!restaurantData) {
-    return <SplashScreen />;
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
   }
 
   return (
     <>
       <StatusBar style="dark" />
       <ParallaxHeader img={restaurantData.img} />
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView>
+        <Text>Hello world</Text>
+      </SafeAreaView>
     </>
   );
 };
