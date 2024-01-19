d2584417fdcbd94f72b80796cfd378bcaeddb9f5
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-19 16:09:10 -0500

    Adds sticky header component for categories

--- components/Parallax/ParallaxHeader.tsx
+++ components/Parallax/ParallaxHeader.tsx
@@ -1,15 +1,22 @@
-import { View, Text, Image, TouchableOpacity, SectionList } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SectionList,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
-
+import Animated from "react-native-reanimated";
 import { useNavigation } from "expo-router";
 
 import { Entypo } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 
 import tw from "twrnc";
+import { Restaurant } from "../../constants/types";
 
 import ParallaxScrollView from "./ParallaxScrollView";
-import { Restaurant } from "../../constants/types";
 import FoodItemCard from "../FoodItemCard";
 
 interface ParallaxHeaderProps {
@@ -56,74 +63,94 @@ const ParallaxHeader = ({ data }: ParallaxHeaderProps) => {
   });
 
   return (
-    <ParallaxScrollView
-      backgroundColor={"#fff"}
-      parallaxHeaderHeight={300}
-      stickyHeaderHeight={110}
-      style={{ flex: 1 }}
-      renderBackground={() => (
-        <Image source={{ uri: data.img }} style={{ width: 400, height: 500 }} />
-      )}
-      renderStickyHeader={() => (
-        <View key="sticky-header" style={tw`ml-20`}>
-          <Text style={tw`text-2xl mt-14`}>{data.name}</Text>
-        </View>
-      )}
-    >
-      <View>
-        <Text style={tw`text-2xl ml-8 mt-2 `}>
-          {data.name}{" "}
-          <Text style={tw`text-base`}>
-            {data.rating}
-            <Entypo name="star" size={16} color="green" /> (
-            {data.numberOfRatings})
+    <>
+      <ParallaxScrollView
+        backgroundColor={"#fff"}
+        parallaxHeaderHeight={300}
+        stickyHeaderHeight={110}
+        style={{ flex: 1 }}
+        renderBackground={() => (
+          <Image
+            source={{ uri: data.img }}
+            style={{ width: 400, height: 500 }}
+          />
+        )}
+        renderStickyHeader={() => (
+          <View key="sticky-header" style={tw`ml-20`}>
+            <Text style={tw`text-2xl mt-14`}>{data.name}</Text>
+          </View>
+        )}
+      >
+        <View>
+          <Text style={tw`text-2xl ml-8 mt-2 `}>
+            {data.name}{" "}
+            <Text style={tw`text-base`}>
+              {data.rating}
+              <Entypo name="star" size={16} color="green" /> (
+              {data.numberOfRatings})
+            </Text>
           </Text>
-        </Text>
-        <Text style={tw`text-sm ml-8 mb-2 text-neutral-500`}>
-          ${data.fee} delivery fee, {data.minDeliveryTime}-
-          {data.maxDeliveryTime} min delivery time
-        </Text>
-        <Text style={tw`text-neutral-600 mx-4 mb-2`}>{data.description}</Text>
-        <SectionList
-          keyExtractor={(item, index) => `${item.name + index}`}
-          scrollEnabled={false}
-          sections={sectionData}
-          renderItem={({ item }) => <FoodItemCard item={item} />}
-          renderSectionHeader={({ section: { title } }) => (
-            <View
-              style={{
-                borderBottomColor: "#000",
-                borderBottomWidth: 2,
-                paddingBottom: 2,
-                paddingHorizontal: 8,
-                marginBottom: 10,
-                marginHorizontal: 20,
-              }}
-            >
-              <Text style={tw`text-2xl font-semibold`}>{title}</Text>
-            </View>
-          )}
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                height: 1,
-                backgroundColor: "#E5E5E5",
-                marginVertical: 4,
-              }}
-            />
-          )}
-          SectionSeparatorComponent={() => (
-            <View
-              style={{
-                height: 1,
-                backgroundColor: "#E5E5E5",
-                marginVertical: 4,
-              }}
-            />
-          )}
-        />
-      </View>
-    </ParallaxScrollView>
+          <Text style={tw`text-sm ml-8 mb-2 text-neutral-500`}>
+            ${data.fee} delivery fee, {data.minDeliveryTime}-
+            {data.maxDeliveryTime} min delivery time
+          </Text>
+          <Text style={tw`text-neutral-600 mx-4 mb-2`}>{data.description}</Text>
+          <SectionList
+            keyExtractor={(item, index) => `${item.name + index}`}
+            scrollEnabled={false}
+            sections={sectionData}
+            renderItem={({ item }) => <FoodItemCard item={item} />}
+            renderSectionHeader={({ section: { title } }) => (
+              <View
+                style={{
+                  borderBottomColor: "#000",
+                  borderBottomWidth: 2,
+                  paddingBottom: 2,
+                  paddingHorizontal: 8,
+                  marginBottom: 10,
+                  marginHorizontal: 20,
+                }}
+              >
+                <Text style={tw`text-2xl font-semibold`}>{title}</Text>
+              </View>
+            )}
+            ItemSeparatorComponent={() => (
+              <View
+                style={{
+                  height: 1,
+                  backgroundColor: "#E5E5E5",
+                  marginVertical: 4,
+                }}
+              />
+            )}
+            SectionSeparatorComponent={() => (
+              <View
+                style={{
+                  height: 1,
+                  backgroundColor: "#E5E5E5",
+                  marginVertical: 4,
+                }}
+              />
+            )}
+          />
+        </View>
+      </ParallaxScrollView>
+      <Animated.View>
+        <View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 16 }}
+          >
+            {sectionData.map((item, index) => (
+              <TouchableOpacity key={index}>
+                <Text>{item.title}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </Animated.View>
+    </>
   );
 };
 
