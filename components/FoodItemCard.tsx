ff3696e5af374266f2d2d5772c14516635810779
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-19 16:00:03 -0500

    Alters layout of food item card to fit when food title is too big

--- components/FoodItemCard.tsx
+++ components/FoodItemCard.tsx
@@ -14,10 +14,8 @@ const FoodItemCard = ({ item }: FoodItemCardProps) => {
     <Link href={"/"} asChild>
       <View style={tw`flex-row justify-between mx-4`}>
         <View>
-          <Text style={tw`text-neutral-700 text-2xl`}>
-            {item.name}{" "}
-            <Text style={tw`text-neutral-500 text-lg`}>${item.price}</Text>
-          </Text>
+          <Text style={tw`text-neutral-700 text-2xl`}>{item.name}</Text>
+          <Text style={tw`text-neutral-500 text-base`}>${item.price}</Text>
           <Text style={tw`text-neutral-400`}>{item.info}</Text>
         </View>
         <Image source={{ uri: item.img }} width={100} height={100} />
