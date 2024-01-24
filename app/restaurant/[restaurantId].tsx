380762a85fc5ccc9873b96392c2758a6c54ef9ae
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-24 18:46:46 -0500

    Displays number of items and total during restaurant menu display

--- app/restaurant/[restaurantId].tsx
+++ app/restaurant/[restaurantId].tsx
@@ -10,12 +10,16 @@ import { Restaurant } from "../../constants/types";
 import ParallaxHeader from "../../components/Parallax/ParallaxHeader";
 import SkeletonHeader from "../../components/Skeleton/SkeletonHeader";
 import SkeletonSection from "../../components/Skeleton/SkeletonSection";
+import RestaurantFooter from "../../components/RestaurantFooter";
+import { useCart } from "../../hooks/useCart";
 
 const RestaurantIdPage = () => {
   const { restaurantId } = useLocalSearchParams();
   const id = restaurantId as string;
 
   const [restaurantData, setRestaurantData] = useState<Restaurant>();
+
+  const { quantity, total } = useCart();
 
   useEffect(() => {
     const db = getFirestore(firebase_app);
@@ -43,6 +47,7 @@ const RestaurantIdPage = () => {
     <>
       <StatusBar style="dark" />
       <ParallaxHeader data={restaurantData} />
+      {quantity > 0 && <RestaurantFooter quantity={quantity} total={total} />}
     </>
   );
 };
---
+++ components/RestaurantFooter.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RestaurantFooter = () => {
+  return (
+    <View>
+      <Text>RestaurantFooter</Text>
+    </View>
+  );
+};
+
+export default RestaurantFooter;
