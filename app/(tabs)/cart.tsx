5a55c973c9cb5a42121501eb93fabf19852e8f3b
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-25 15:54:18 -0500

    Adds header component and item seperator component

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,6 +1,10 @@
-import { FlatList, SafeAreaView } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 import React from "react";
+
+import tw from "twrnc";
+
 import { useCart } from "../../hooks/useCart";
+
 import ProductCard from "../../components/ProductCard";
 
 const Cart = () => {
@@ -11,6 +15,10 @@ const Cart = () => {
     <SafeAreaView>
       <FlatList
         data={products}
+        ListHeaderComponent={<Text style={tw`text-xl my-2 pl-4`}>Items</Text>}
+        ItemSeparatorComponent={() => (
+          <View style={{ height: 1, backgroundColor: "#FCFCFC" }} />
+        )}
         renderItem={({ item }) => (
           <ProductCard
             item={item}
