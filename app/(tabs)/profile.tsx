1506a3b3f8b1e52cf19e6175410f3770a33d103a
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-28 22:40:50 -0500

    Creates base layout for profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,31 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import React from "react";
+
+import { FontAwesome, Feather, Entypo } from "@expo/vector-icons";
+
+import tw from "twrnc";
 
 const Profile = () => {
   return (
     <SafeAreaView>
-      <Text>ProfileScreen</Text>
+      <View style={tw`flex-row mx-4`}>
+        <Text>Name</Text>
+        <FontAwesome name="user" size={24} color="black" />
+      </View>
+      <View style={tw`flex-row`}>
+        <View>
+          <Entypo name="price-tag" size={24} color="black" />
+          <Text>Past Orders</Text>
+        </View>
+        <View>
+          <Entypo name="credit-card" size={24} color="black" />
+          <Text>Payment Options</Text>
+        </View>
+        <View>
+          <Entypo name="price-tag" size={24} color="black" />
+          <Text>Past Orders</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
