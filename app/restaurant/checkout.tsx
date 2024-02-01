8868b212d0edd235dce3a09bc0a9cdbec65bc2ef
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-01 15:34:40 -0500

    Styles checkout page and redirects user to create a new order at the home page

--- app/restaurant/checkout.tsx
+++ app/restaurant/checkout.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+
+import tw from "twrnc";
+import { Link } from "expo-router";
 
 const checkout = () => {
   return (
-    <View>
-      <Text>checkout</Text>
+    <View style={tw`h-screen items-center justify-center`}>
+      <Text style={tw`text-3xl font-bold`}>Thanks for your order!</Text>
+      <Link href={"/(tabs)/home"} asChild>
+        <TouchableOpacity style={tw`bg-green-700 px-4 py-2 rounded-lg`}>
+          <Text style={tw`text-white font-semibold`}>New Order</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 };
