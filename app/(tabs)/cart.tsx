5361081c9022b099b277a04c19c102b724a46fc2
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-01 15:11:29 -0500

    Changes layout for all of cart page to be scrollable

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -54,26 +54,31 @@ const Cart = () => {
 
   return (
     <View style={tw`flex-1`}>
-      <View style={tw`gap-4 `}>
-        <Text style={tw`text-2xl mt-2 mx-4`}>
-          {restaurantOrderName} - Delivery
-        </Text>
-        <View style={tw`flex-row bg-white py-4 px-4 border border-gray-400`}>
-          <MaterialIcons name="delivery-dining" size={32} color="black" />
-          <Text style={tw`flex-1 mx-2`}>
-            Deliver from {getDeliveryTime(deliveryTime).join(" - ")} minutes{" "}
-          </Text>
-          <Text style={tw`text-green-600 text-xl`}>Change</Text>
-        </View>
-        <ToggleFeature
-          title="Add Cutlery?"
-          description="Help save the environment and cut down on environmental waste!"
-        />
-      </View>
       <FlatList
         data={products}
         ListHeaderComponent={() => (
-          <Text style={tw`flex-1 text-3xl mt-2 mb-2 pl-4`}>Items</Text>
+          <>
+            <View style={tw`gap-4 `}>
+              <Text style={tw`text-2xl mt-2 mx-4`}>
+                {restaurantOrderName} - Delivery
+              </Text>
+              <View
+                style={tw`flex-row bg-white py-4 px-4 border border-gray-400`}
+              >
+                <MaterialIcons name="delivery-dining" size={32} color="black" />
+                <Text style={tw`flex-1 mx-2`}>
+                  Deliver from {getDeliveryTime(deliveryTime).join(" - ")}{" "}
+                  minutes{" "}
+                </Text>
+                <Text style={tw`text-green-600 text-xl`}>Change</Text>
+              </View>
+              <ToggleFeature
+                title="Add Cutlery?"
+                description="Help save the environment and cut down on environmental waste!"
+              />
+            </View>
+            <Text style={tw`flex-1 text-3xl mt-2 mb-2 pl-4`}>Items</Text>
+          </>
         )}
         ItemSeparatorComponent={() => (
           <View
@@ -88,25 +93,25 @@ const Cart = () => {
           />
         )}
         ListFooterComponent={
-          <View style={tw`bg-white mx-4`}>
-            <View style={tw`flex-row justify-between mx-2 mt-4`}>
+          <View style={tw`bg-white mx-4 mb-32 mt-2 gap-4 py-2`}>
+            <View style={tw`flex-row justify-between mx-2`}>
               <Text style={tw`text-xl text-neutral-400`}>Subtotal</Text>
               <Text style={tw`text-lg`}>${total.toFixed(2)}</Text>
             </View>
-            <View style={tw`flex-row justify-between mx-2 mt-4`}>
+            <View style={tw`flex-row justify-between mx-2`}>
               <Text style={tw`text-xl text-neutral-400`}>Service Fee</Text>
               <Text style={tw`text-lg`}>$2.99</Text>
             </View>
-            <View style={tw`flex-row justify-between mx-2 mt-4`}>
+            <View style={tw`flex-row justify-between mx-2`}>
               <Text style={tw`text-xl text-neutral-400`}>Delivery Fee</Text>
               <Text style={tw`text-lg`}>${(total * 1.13).toFixed(2)}</Text>
             </View>
-            <View style={tw`flex-row justify-between mx-2 mt-4`}>
+            <View style={tw`flex-row justify-between mx-2`}>
               <Text style={tw`text-xl text-neutral-400`}>Tax</Text>
               <Text style={tw`text-lg`}>${fee}</Text>
             </View>
-            <View style={tw`flex-row justify-between mx-2 mt-4`}>
-              <Text style={tw`text-xl text-neutral-400`}>Delivery Fee</Text>
+            <View style={tw`flex-row justify-between mx-2`}>
+              <Text style={tw`text-xl text-neutral-400`}>Total</Text>
               <Text style={tw`text-lg`}>
                 ${(total + 2.99 + fee + total * 1.13).toFixed(2)}
               </Text>
