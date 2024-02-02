cd152a3447f1b8a2c5e10cd6f6d5066d11a9cfa0
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-02 15:30:29 -0500

    Creates scrollable settings list using flat list to render all settings moves header component into flat list header

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,23 +1,43 @@
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, FlatList } from "react-native";
 import React from "react";
 
 import { FontAwesome } from "@expo/vector-icons";
-
 import tw from "twrnc";
+
+import { Setting } from "../../constants/types";
+
 import IconCard from "../../components/Cards/IconCard";
+import SettingRow from "../../components/SettingRow";
+
+const settingsData: Array<Setting> = [
+  { fontAwesomeIconName: "info-circle", name: "About" },
+];
 
 const Profile = () => {
   return (
     <SafeAreaView>
-      <View style={tw`flex-row mx-4`}>
-        <Text>Name</Text>
-        <FontAwesome name="user" size={24} color="black" />
-      </View>
-      <View style={tw`flex-row`}>
-        <IconCard entypoIconName="shopping-basket" label="Past Orders" />
-        <IconCard entypoIconName="credit-card" label="Payment Options" />
-        <IconCard entypoIconName="price-tag" label="Offers and Coupons" />
-      </View>
+      <FlatList
+        data={settingsData}
+        ListHeaderComponent={() => (
+          <>
+            <View style={tw`flex-row px-6 py-2 justify-between mt-4 bg-white`}>
+              <Text style={tw`text-2xl`}>Name</Text>
+              <FontAwesome name="user" size={40} color="black" />
+            </View>
+            <View style={tw`flex-row justify-between mx-4 gap-2 mt-4`}>
+              <IconCard entypoIconName="shopping-basket" label="Past Orders" />
+              <IconCard entypoIconName="wallet" label="Wallet" />
+              <IconCard entypoIconName="price-tag" label="Offers" />
+            </View>
+          </>
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{ height: 1, backgroundColor: "#FCFCFC", marginVertical: 4 }}
+          />
+        )}
+        renderItem={({ item }) => <SettingRow setting={item} />}
+      />
     </SafeAreaView>
   );
 };
