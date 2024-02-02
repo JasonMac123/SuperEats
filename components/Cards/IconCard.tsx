db3ddc2d592e04296ec1d4da04c33607f13fe690
JasonSnow123 <m.jason.2018@gmail.com>
2024-02-02 15:31:03 -0500

    Styles IconCard to be flush with layout

--- components/Cards/IconCard.tsx
+++ components/Cards/IconCard.tsx
@@ -3,6 +3,8 @@ import React from "react";
 
 import { Entypo } from "@expo/vector-icons";
 
+import tw from "twrnc";
+
 interface IconCardProps {
   entypoIconName: keyof typeof Entypo.glyphMap;
   label: string;
@@ -10,9 +12,11 @@ interface IconCardProps {
 
 const IconCard = ({ entypoIconName, label }: IconCardProps) => {
   return (
-    <View>
-      <Entypo name={entypoIconName} />
-      <Text>{label}</Text>
+    <View
+      style={tw`bg-white items-center justify-center rounded-xl w-28 h-30 py-4`}
+    >
+      <Entypo name={entypoIconName} size={35} />
+      <Text style={tw`mt-2 mx-4`}>{label}</Text>
     </View>
   );
 };
